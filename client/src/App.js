c94e632ebdcc1c4e12540777c8e9d1dca74775be
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-10-28 22:54:55 +0200

    listing years awards

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-    const [backendData, setBackendData] = useState();
+    const [yearsLinkList, setYearsLinkList] = useState();
+    const [yearAward, setYearAward] = useState("2021 AWARDS");
+    // const [backendData, setBackendData] = useState();
+
     useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setBackendData(data);
-            });
+        (async () => {
+            const res = await fetch("/years-list");
+            const data = await res.json();
+            setYearsLinkList(data);
+        })();
     }, []);
 
+    const handleYears = async (e) => {
+        console.log("e", e);
+        const yearChoice = setYearAward(e.target.textContent);
+
+        console.log("year", e.target.textContent);
+    };
     return (
         <div>
-            {backendData &&
-                backendData.users.map((user, i) => <p key={i}>{user}</p>)}
+            <h3>This is the {yearAward} genres book list</h3>
+            <h6>if you like you can choose another year</h6>
+            {yearsLinkList &&
+                yearsLinkList.map((year, i) => (
+                    <h6
+                        value={year.title}
+                        id={year.link}
+                        onClick={(e) => {
+                            handleYears(e);
+                        }}
+                    >
+                        {year.title}
+                    </h6>
+                ))}
+            <button> </button>
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const { launch } = require("puppeteer");
 
-app.get("/api", (req, res) => {
+app.get("/api", async (req, res) => {
     res.json({ users: ["user", "another user", "third user"] });
+});
+app.get("/years-list", async (req, res) => {
+    const browser = await launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.goodreads.com/choiceawards/best-books-2021");
+    const yearsList = await page.$$eval(
+        "#previousYears > ul > li > a",
+        (yearsList) => {
+            return yearsList.map((year) => ({
+                title: year.innerHTML,
+                link: year.getAttribute("href"),
+            }));
+        }
+    );
+
+    res.json(yearsList);
 });
 
 app.listen(3001, () => {
