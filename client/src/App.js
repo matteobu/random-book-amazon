fb81dd68bd35c726c508f4d0f7e53491608ed27d
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-10-28 20:36:02 +0200

    connection FE BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-
-
-
-
+import React, { useState, useEffect } from "react";
 
 function App() {
-    return <div className="App">HALL</div>;
+    const [backendData, setBackendData] = useState();
+    useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setBackendData(data);
+            });
+    }, []);
+
+    return (
+        <div>
+            {backendData &&
+                backendData.users.map((user, i) => <p key={i}>{user}</p>)}
+        </div>
+    );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
 
 app.get("/api", (req, res) => {
-    res.json({ message: "api connected" });
+    res.json({ users: ["user", "another user", "third user"] });
 });
 
-app.listen(PORT, () => {
-    console.log("server is running on port:", PORT);
+app.listen(3001, () => {
+    console.log("The server is listening");
 });
